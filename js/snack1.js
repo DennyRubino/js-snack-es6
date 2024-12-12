04e4e86c0fd55f3dc083499a237268663db0633a
Denny Rubino <rubidenny@gmail.com>
2024-12-12 17:15:27 +0100

    add snack 1

--- js/snack1.js
+++ js/snack1.js
@@ -2,3 +2,31 @@
 //Creare un array di oggetti:
 //Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 //Stampare a schermo la bici con peso minore.
+
+const bike =[ {
+    'nome' : '390 Duke',
+    'peso' : '165',
+
+},
+{
+    'nome' : '790 Duke',
+    'peso' : '169',
+},
+{
+    'nome' : '1290 Duke',
+    'peso' : '191',
+}
+]
+
+let lightestBike = bike[0];
+
+for (let i = 0; i < bike.length; i++) 
+{ if (bike[i].peso < lightestBike.peso) {
+        lightestBike = bike[i];}
+}
+console.log(lightestBike);
+
+    
+
+
+
